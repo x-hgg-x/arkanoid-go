60d922bec80a32de384bdcbe747eb071b89468ce
x-hgg-x <39058530+x-hgg-x@users.noreply.github.com>
2020-02-22 12:01:35 +0100

    Add helper function for adding entity with components

--- main.go
+++ main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"log"
+	"reflect"
 
 	c "arkanoid/components"
 	e "arkanoid/ecs"
 	"arkanoid/systems/sprite"
 
+	"github.com/ByteArena/ecs"
 	"github.com/hajimehoshi/ebiten"
 	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
@@ -34,6 +36,14 @@ func (g game) Update(screen *ebiten.Image) error {
 	sprite.RenderSystem(g.ecs, screen)
 
 	return nil
+}
+
+func addEntity(entity *ecs.Entity, ecsComponentList *c.Components, components []interface{}) {
+	for _, component := range components {
+		componentName := reflect.ValueOf(component).Elem().Type().Name()
+		ecsComponent := reflect.ValueOf(ecsComponentList).Elem().FieldByName(componentName).Interface().(*ecs.Component)
+		entity.AddComponent(ecsComponent, component)
+	}
 }
 
 func main() {
@@ -70,40 +80,38 @@ func main() {
 		},
 	}
 
-	ecs := e.InitEcs()
+	ecsData := e.InitEcs()
 
-	ecs.Manager.NewEntity().
-		AddComponent(ecs.Components.SpriteRender,
-			&c.SpriteRender{
-				SpriteSheet:  &backgroundSpriteSheet,
-				SpriteNumber: 0,
-				Options:      &ebiten.DrawImageOptions{},
-			}).
-		AddComponent(ecs.Components.Transform, c.NewTransform().SetTranslation(360, 300).SetDepth(-1))
+	backgroundSpriteRender := &c.SpriteRender{
+		SpriteSheet:  &backgroundSpriteSheet,
+		SpriteNumber: 0,
+		Options:      &ebiten.DrawImageOptions{},
+	}
+	backgroundTransform := c.NewTransform().SetTranslation(360, 300).SetDepth(-1)
 
-	ecs.Manager.NewEntity().
-		AddComponent(ecs.Components.SpriteRender,
-			&c.SpriteRender{
-				SpriteSheet:  &gameSpriteSheet,
-				SpriteNumber: 0,
-				Options:      &ebiten.DrawImageOptions{},
-			}).
-		AddComponent(ecs.Components.Transform, c.NewTransform().SetTranslation(360, 12))
+	paddleSpriteRender := &c.SpriteRender{
+		SpriteSheet:  &gameSpriteSheet,
+		SpriteNumber: 0,
+		Options:      &ebiten.DrawImageOptions{},
+	}
+	paddleTransform := c.NewTransform().SetTranslation(360, 12)
 
-	ecs.Manager.NewEntity().
-		AddComponent(ecs.Components.SpriteRender,
-			&c.SpriteRender{
-				SpriteSheet:  &gameSpriteSheet,
-				SpriteNumber: 1,
-				Options:      &ebiten.DrawImageOptions{},
-			}).
-		AddComponent(ecs.Components.Transform, c.NewTransform().SetTranslation(360, 35).SetDepth(0.2))
+	ballSpriteRender := &c.SpriteRender{
+		SpriteSheet:  &gameSpriteSheet,
+		SpriteNumber: 1,
+		Options:      &ebiten.DrawImageOptions{},
+	}
+	ballTransform := c.NewTransform().SetTranslation(360, 35).SetDepth(0.2)
+
+	addEntity(ecsData.Manager.NewEntity(), ecsData.Components, []interface{}{backgroundSpriteRender, backgroundTransform})
+	addEntity(ecsData.Manager.NewEntity(), ecsData.Components, []interface{}{paddleSpriteRender, paddleTransform})
+	addEntity(ecsData.Manager.NewEntity(), ecsData.Components, []interface{}{ballSpriteRender, ballTransform})
 
 	ebiten.SetWindowResizable(true)
 	ebiten.SetWindowSize(windowWidth, windowHeight)
 	ebiten.SetWindowTitle("Arkanoid")
 
-	if err := ebiten.RunGame(game{ecs}); err != nil {
+	if err := ebiten.RunGame(game{ecsData}); err != nil {
 		log.Fatal(err)
 	}
 }
